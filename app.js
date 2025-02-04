aedbe3e765052c659de1c07fdaf67b38faded2ce
patitobermhellooo <fgengelgardenlife@gmail.com>
2025-02-04 14:09:13 -0300

    Corección: ahora el servidor usa el puerto asignado por Render

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ const PORT = process.env.PORT || 3000 || "0.0.0.0";
     try {
       await sequelize.authenticate();
       console.log('✅ Servidor conectado a la base de datos.');
-      app.listen(3000, () => console.log('🚀 Servidor corriendo en http://localhost:3000'));
+      app.listen(PORT, "0.0.0.0", () => console.log('🚀 Servidor corriendo en http://localhost:3000'));
     } catch (error) {
       console.error('❌ No se pudo conectar a la base de datos:', error);
     }
